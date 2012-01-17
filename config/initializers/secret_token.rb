0df8248374e556373d8eaa2ab3146e855bf1510a
Laser Nite <lasernite@Lasers-MacBook-Pro.local>
2012-01-17 11:08:07 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '29bb0a067006e1d505fd7bee6fe45c0681244c7e3498b69808a6ead3cf316288b144e8905c11ba5740a4d27834b8ed69f8c59b27d1a6a20451404e35305f45c6'
